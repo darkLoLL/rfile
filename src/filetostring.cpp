e79e7f2168e28cbd160da4d0ee7fd5b195a0aea4
DarkLoLL <2607749572@qq.com>
2023-02-05 05:50:06 +0800

    add c

--- src/filetostring.cpp
+++ src/filetostring.cpp
@@ -3,6 +3,24 @@
 #include <sstream>
 #include <string>
 
+void fun(char* fname, char** str){
+    FILE* file;
+    long size, i;
+    file = fopen(fname, "r");
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    *str = (char*)malloc(size + 1);
+    i = 0;
+    fseek(file, 0, SEEK_SET);
+    while (!feof(file))
+    {
+        (*str)[i] = getc(file);
+        i++;
+    }
+    (*str)[size] = '\0';
+    fclose(file);
+}
+
 std::string rfilestr(std::string file_name) {
   std::stringstream str;
   char c;
